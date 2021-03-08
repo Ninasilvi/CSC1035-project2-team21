9fb793c7fb63748b250961c9842980387cb70a84
Ninasilvi <44772927+Ninasilvi@users.noreply.github.com>
2021-03-08 18:53:35 +0000

    Added ModuleRequirements fields

--- src/main/java/csc1035/project2/ModuleRequirements.java
+++ src/main/java/csc1035/project2/ModuleRequirements.java
@@ -1,4 +1,29 @@
 package csc1035.project2;
 
+import javax.persistence.*;
+
+@Table(name = "ModuleRequirements")
 public class ModuleRequirements {
+
+    @Id
+    @Column
+    private String moduleID;
+
+    @Column
+    private String weekCommencing;
+
+    @Column
+    private int lecturesPerWeek;
+
+    @Column
+    private int lectureLength;
+
+    @Column
+    private int practicalsPerWeek;
+
+    @Column
+    private int practicalLength;
+
+    public ModuleRequirements() {}
+
 }
