81ff8a7aa518ec7fd1aad57fa3cc7df6e1e86a14
Abdulla <abdullamurshed11@outlook.com>
2021-03-10 17:09:26 +0000

    added constructors and methods.

--- src/main/java/csc1035/project2/ModuleRequirements.java
+++ src/main/java/csc1035/project2/ModuleRequirements.java
@@ -26,4 +26,52 @@ public class ModuleRequirements {
 
     public ModuleRequirements() {}
 
+
+public ModuleRequirements(String moduleID, String weekCommencing, int lecturesPerWeek, int practicalsPerWeek, int lectureLength, int practicalLength){
+    this.moduleID = moduleID;
+    this.weekCommencing = weekCommencing;
+    this.lecturesPerWeek = lecturesPerWeek;
+    this.practicalsPerWeek = practicalsPerWeek;
+    this.lectureLength = lectureLength;
+    this.practicalLength = practicalLength;
+}
+
+public void ModuleRequirement(){}
+
+public String getModuleID(){
+    return moduleID;
+}
+public void setModuleID(String moduleID){
+    this.moduleID = moduleID;
+}
+public String getWeekCommencing(){
+    return weekCommencing;
+}
+public void setWeekCommencing(String weekCommencing){
+    this.weekCommencing = weekCommencing;
+}
+public int getLecturesPerWeek(){
+    return lecturesPerWeek;
+}
+public void setLecturesPerWeek(int lecturesPerWeek){
+    this.lecturesPerWeek = lecturesPerWeek;
+}
+public int getPracticalsPerWeek(){
+    return practicalsPerWeek;
+}
+public void setPracticalsPerWeek(int practicalsPerWeek){
+    this.practicalsPerWeek = practicalsPerWeek;
+}
+public int getLectureLength(){
+    return lectureLength;
+}
+public void setLectureLength(int lectureLength){
+    this.lectureLength = lectureLength;
+}
+public int getPracticalLength(){
+    return practicalLength;
+}
+public void setPracticalLength(){
+    this.practicalLength = practicalLength;
+}
 }
