2e91d49dc6abfb4271b6fad9066ee77a02378422
Sarunas Martinaitis <c0051496@newcastle.ac.uk>
2021-03-11 16:43:00 +0200

    Added Timetable.java

---
+++ src/main/java/csc1035/project2/Timetable.java
@@ -0,0 +1,35 @@
+package csc1035.project2;
+
+public class Timetable {
+    public static void main(String[] args) {
+
+
+    }
+
+    // It has a list of Students that take a Module
+    public void listOfStudents() {
+
+
+
+    }
+
+    // It has a list of staff that teach a module
+    public void listOfStaff() {
+
+    }
+
+    // It has a list of module requirements
+    public void listOfModuleReq() {
+
+    }
+
+    // Allows the admin to create a timetable (and book relevant rooms) for the school
+    public void allowCreateTimetable() {
+
+    }
+
+    // A way of producing a timetable for a staff member or student
+    public void producingTimetable() {
+
+    }
+}
