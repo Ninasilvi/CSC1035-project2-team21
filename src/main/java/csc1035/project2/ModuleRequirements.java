d4787011eadbcbba70a2438884bd2d8a5bb93416
Sarunas Martinaitis <c0051496@newcastle.ac.uk>
2021-03-14 14:04:09 +0200

    Added ToString() to ModuleRequirements.java

--- src/main/java/csc1035/project2/ModuleRequirements.java
+++ src/main/java/csc1035/project2/ModuleRequirements.java
@@ -3,6 +3,7 @@ package csc1035.project2;
 import javax.persistence.*;
 
 @Table(name = "ModuleRequirements")
+@javax.persistence.Entity(name = "ModuleRequirements")
 public class ModuleRequirements {
 
     @Id
@@ -27,51 +28,56 @@ public class ModuleRequirements {
     public ModuleRequirements() {}
 
 
-public ModuleRequirements(String moduleID, String weekCommencing, int lecturesPerWeek, int practicalsPerWeek, int lectureLength, int practicalLength){
-    this.moduleID = moduleID;
-    this.weekCommencing = weekCommencing;
-    this.lecturesPerWeek = lecturesPerWeek;
-    this.practicalsPerWeek = practicalsPerWeek;
-    this.lectureLength = lectureLength;
-    this.practicalLength = practicalLength;
-}
+    public ModuleRequirements(String moduleID, String weekCommencing, int lecturesPerWeek, int practicalsPerWeek, int lectureLength, int practicalLength){
+        this.moduleID = moduleID;
+        this.weekCommencing = weekCommencing;
+        this.lecturesPerWeek = lecturesPerWeek;
+        this.practicalsPerWeek = practicalsPerWeek;
+        this.lectureLength = lectureLength;
+        this.practicalLength = practicalLength;
+    }
 
-public void ModuleRequirement(){}
+    public void ModuleRequirement(){}
 
-public String getModuleID(){
-    return moduleID;
-}
-public void setModuleID(String moduleID){
-    this.moduleID = moduleID;
-}
-public String getWeekCommencing(){
-    return weekCommencing;
-}
-public void setWeekCommencing(String weekCommencing){
-    this.weekCommencing = weekCommencing;
-}
-public int getLecturesPerWeek(){
-    return lecturesPerWeek;
-}
-public void setLecturesPerWeek(int lecturesPerWeek){
-    this.lecturesPerWeek = lecturesPerWeek;
-}
-public int getPracticalsPerWeek(){
-    return practicalsPerWeek;
-}
-public void setPracticalsPerWeek(int practicalsPerWeek){
-    this.practicalsPerWeek = practicalsPerWeek;
-}
-public int getLectureLength(){
-    return lectureLength;
-}
-public void setLectureLength(int lectureLength){
-    this.lectureLength = lectureLength;
-}
-public int getPracticalLength(){
-    return practicalLength;
-}
-public void setPracticalLength(){
-    this.practicalLength = practicalLength;
-}
+    public String getModuleID(){
+        return moduleID;
+    }
+    public void setModuleID(String moduleID){
+        this.moduleID = moduleID;
+    }
+    public String getWeekCommencing(){
+        return weekCommencing;
+    }
+    public void setWeekCommencing(String weekCommencing){
+        this.weekCommencing = weekCommencing;
+    }
+    public int getLecturesPerWeek(){
+        return lecturesPerWeek;
+    }
+    public void setLecturesPerWeek(int lecturesPerWeek){
+        this.lecturesPerWeek = lecturesPerWeek;
+    }
+    public int getPracticalsPerWeek(){
+        return practicalsPerWeek;
+    }
+    public void setPracticalsPerWeek(int practicalsPerWeek){
+        this.practicalsPerWeek = practicalsPerWeek;
+    }
+    public int getLectureLength(){
+        return lectureLength;
+    }
+    public void setLectureLength(int lectureLength){
+        this.practicalsPerWeek = lectureLength;
+    }
+    public int getPracticalLength(){
+        return practicalLength;
+    }
+    public void setPracticalLength(){
+        this.practicalLength = practicalLength;
+    }
+
+    @Override
+    public String toString() {
+        return moduleID + " " + weekCommencing  + " " + lecturesPerWeek + " " + practicalsPerWeek + " " + practicalsPerWeek + " " + practicalLength;
+    }
 }
