e7adcece3a22d89c4ff423c41f10fa462547c80f
Sarunas Martinaitis <c0051496@newcastle.ac.uk>
2021-03-14 13:56:17 +0200

    Added StaffModule.java class

---
+++ src/main/java/csc1035/project2/StaffModule.java
@@ -0,0 +1,43 @@
+package csc1035.project2;
+
+import javax.persistence.*;
+
+@Table(name = "StaffModule")
+@javax.persistence.Entity(name = "StaffModule")
+public class StaffModule {
+
+    @Id
+    @Column(updatable = false, nullable = false)
+    private String staffID;
+
+    @Column
+    private String moduleID;
+
+    public StaffModule(String staffID, String moduleID) {
+        this.staffID = staffID;
+        this.moduleID = moduleID;
+    }
+
+    public StaffModule() {}
+
+    public String getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(String staffID) {
+        this.staffID = staffID;
+    }
+
+    public String getModuleID() {
+        return moduleID;
+    }
+
+    public void setModuleID(String moduleID) {
+        this.moduleID = moduleID;
+    }
+
+    @Override
+    public String toString() {
+        return staffID + " " + moduleID;
+    }
+}
