297e0a22994b84ea2048b19e82121d2d94dbc55c
Sarunas Martinaitis <c0051496@newcastle.ac.uk>
2021-03-14 12:59:01 +0200

    Created 'listOfStudents' method

--- src/main/java/csc1035/project2/Timetable.java
+++ src/main/java/csc1035/project2/Timetable.java
@@ -1,16 +1,81 @@
 package csc1035.project2;
+import org.hibernate.Session;
+
+import javax.persistence.Query;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Timetable {
     public static void main(String[] args) {
 
+        Scanner s = new Scanner(System.in);
+        Timetable t = new Timetable();
 
+        while(true) {
+            t.printMenu();
+            int choice = s.nextInt();
+
+            switch (choice) {
+                case 1:
+                    t.listOfStudents();
+                    break;
+                case 2:
+                    t.listOfStaff();
+                    break;
+                case 3:
+                    t.listOfModuleReq();
+                    break;
+                case 4:
+                    t.allowCreateTimetable();
+                    break;
+                case 5:
+                    t.producingTimetable();
+                    break;
+                case 6:
+                    return;
+            }
+        }
     }
 
     // It has a list of Students that take a Module
     public void listOfStudents() {
 
+        Scanner s = new Scanner(System.in);
+        Session se = HibernateUtil.getSessionFactory().openSession();
 
+        se.beginTransaction();
 
+        List<Module> modules = se.createQuery("FROM Module").list();
+
+        se.getTransaction().commit();
+
+        // Print results
+        System.out.println("Select one Module:");
+        int i = 1;
+        for (Module item : modules) {
+            System.out.println(i + " - " + item);
+            i += 1;
+        }
+
+        int choice = s.nextInt();
+
+        String ModuleID = "'" + modules.get(choice-1).getModuleID() + "'";
+
+        se.beginTransaction();
+
+        String hql = "FROM Student S " +
+                "WHERE S.studentID IN (SELECT SM.studentID FROM StudentModule SM WHERE SM.moduleID = " + ModuleID + ")";
+        List<Student> students = se.createQuery(hql).list();
+        se.getTransaction().commit();
+
+        // Print results
+        for(i = 0; i < students.size(); i++) {
+            System.out.println(i+1 + " - ID: " + students.get(i).getStudentID() + " | First Name: " + students.get(i).getFirstName() + " | Last Name: " + students.get(i).getLastName());
+        }
+        System.out.println("");
+        se.close();
     }
 
     // It has a list of staff that teach a module
@@ -31,5 +96,16 @@ public class Timetable {
     // A way of producing a timetable for a staff member or student
     public void producingTimetable() {
 
+    }
+
+    // Print Menu
+    public void printMenu() {
+        System.out.println("\n Enter an Option [1-6]");
+        System.out.println("1 - List Of Students");
+        System.out.println("2 - List of Staff");
+        System.out.println("3 - List of Module Requirements");
+        System.out.println("4 - Allow Create Timetable");
+        System.out.println("5 - Producing Timetable");
+        System.out.println("6 - Exit \n");
     }
 }
