1eb613e13384964454eb44de4dbc8fea9029c542
c0029698 <s.kirkup2@newcastle.ac.uk>
2021-03-16 15:07:23 +0000

    Changed data type of roomnumber to float in SetUpDatabase

--- src/main/java/csc1035/project2/SetUpDatabase.java
+++ src/main/java/csc1035/project2/SetUpDatabase.java
@@ -29,7 +29,7 @@ public class SetUpDatabase {
         while (sc.hasNextLine()) {
 
             String[] line = sc.nextLine().split(",");
-            rooms.add(new Rooms(line[0], line[1], Integer.parseInt(line[2]), Integer.parseInt(line[3])));
+            rooms.add(new Rooms(Float.parseFloat(line[0]), line[1], Integer.parseInt(line[2]), Integer.parseInt(line[3])));
         }
 
         try {
